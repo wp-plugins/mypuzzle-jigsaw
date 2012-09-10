fdc1794fe40cc26a611489d9279a43f104c952ad
Thomas Seidel <Thomas Seidel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-10 21:38:02 +0000

    - change image compression to 100% to get full image quality
    git-svn-id: https://plugins.svn.wordpress.org/mypuzzle-jigsaw/trunk@597174 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jigsaw-plugin.php
+++ jigsaw-plugin.php
@@ -96,7 +96,7 @@ class jigsaw_mp_simpleImage {
         fclose($fp);     
         return false;
    }
-   function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
+   function save($filename, $image_type=IMAGETYPE_JPEG, $compression=100, $permissions=null) {
  
       if( $image_type == IMAGETYPE_JPEG ) {
          imagejpeg($this->image,$filename,$compression);
